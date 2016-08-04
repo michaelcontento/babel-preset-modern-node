13cf0ac8d9ce79a26e4eae10c4786f1fae0a54e9
Michael Contento <michaelcontento@gmail.com>
2016-08-04 22:08:25 +0200

    add support for v6.3

--- 6.2.js
+++ 6.2.js
@@ -1,14 +1,9 @@
 // V8: 5.0.71.47
 module.exports = {
     presets: [
-        require('./extras')
+        require('./6.3')
     ],
     plugins: [
-        require('babel-plugin-transform-es2015-destructuring'),
-        require('babel-plugin-transform-es2015-function-name'),
-        require('babel-plugin-transform-es2015-modules-commonjs'),
-        require('babel-plugin-transform-es2015-duplicate-keys'),
-        require('babel-plugin-transform-exponentiation-operator'),
-        require('babel-plugin-array-includes').default,
+        require('babel-plugin-array-includes').default
     ]
 }
